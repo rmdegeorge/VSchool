08982b9be88d6eca1e141251c86176ebbb8da484
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-01 19:50:01 -0600

    added completed exercise hello class

---
+++ exercises/hello-class/main.js
@@ -0,0 +1,25 @@
+/* //Code Given in exercise
+var sayHello = function() {
+    console.log("hello class");
+}
+*/
+
+/* //Task Level one
+function sayHello() {
+    console.log("hello class");
+}
+sayHello();
+*/
+
+/* //Task Level two
+function sayHello(name) {
+    console.log(`Hello ${name}!`)
+}
+sayHello("Rob")
+*/
+
+//Task Level three
+function sayHello(name,age) {
+    console.log(`Hello ${name}! Damn, you look good for being ${age}!`)
+}
+sayHello("Rob",31)
