14bde8278afef33e3637147334f03fd6279d845e
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-04 11:07:39 -0600

    completed no-duplicates

--- exercises/week01/no-duplicates/app.js
+++ exercises/week01/no-duplicates/app.js
@@ -1,14 +1,34 @@
-const myString = "Robert DeGeorge"
+const myString = "Robert DeGeorge";
+const myString2 = "aabbcddee";
+const myString3 = "aaaabbcdddeeffghhijjjjklllmnoooop"
 
-function removeDuplicateChars(input) {
 
+//--------------------------------------------------
+function findUniqueChars(string) {
+    let chars = [string[0]];
+    for (let i = 0; i < string.length; i++) {
+        if (!chars.includes(string[i])) {
+            chars.push(string[i]);
+        }
+    }
+    return chars;
 }
+// "fat arrow" function that joins the array returned in findUniqueChars as a single string
+const removeDuplicateChars = string => {return findUniqueChars(string).join("")};
+console.log(removeDuplicateChars(myString3));
+
+
+
+//--------------------------------------------------
 function removeRandomChar(input) {
     // replace char at random index between 0 and length of input string with empty string
     return input.replace(input[Math.round(Math.random() * input.length)], "");
 }
 console.log(removeRandomChar(myString))
 
+
+
+//--------------------------------------------------
 function scrambleString(input) {
     let splitString = input.split("");
     let length = splitString.length;
@@ -23,12 +43,43 @@ function scrambleString(input) {
 }
 console.log(scrambleString(myString))
 
-function removeVowels(input) {
-    let vowels = [a, e, i, o, u];
-    for (let i = 0; i < vowels.length; i++) {
-        //input.indexOf(vowels[i]);
+
+//--------------------------------------------------
+function findIndexOfVowels(input){
+    let indexLog = []
+    // loop through input and log index of vowels
+    for (let i = 0; i < input.length; i++) {
+        let char = input[i];
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u" || char === "A" || char === "E" || char === "I" || char === "O" || char === "U") {
+            indexLog.push(i);
+        }
     }
+    return indexLog;
 }
+function replaceVowels(input,indexLog) {
+    // loop through array & replace vowels at indexes from indexLog with placeholder ~~replaceMe~~
+    let array = input.split("")
+    for (let i = 0; i < indexLog.length; i++) {
+        array.splice(indexLog[i], 1, "~~replaceMe~~")
+    }
+    return array.join("");
+}
+function removePlaceholders(input,length) {
+    //remove all placeholders.  THERE HAS TO BE A BETTER/SIMPLER WAY FOR ALL THIS
+    for (let i = 0; i < length; i++) {
+        input = input.replace("~~replaceMe~~", "");
+    }
+    return input;
+}
+
+function removeVowels(input) {
+    let indexLog = findIndexOfVowels(input);
+    let newString = replaceVowels(input,indexLog);
+    let finalString = removePlaceholders(newString,indexLog.length);
+    return finalString;
+
+}
+console.log(removeVowels(myString));
 /*
     Given a string, remove any duplicate letters.
     ///Create a function that randomly removes a letter.
