48ef844a1db85b777541c4c471c972ea42b88a7e
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-07 09:12:07 -0600

    Started: design-a-blog, dom-olympics, uptown-function-you-up

---
+++ exercises/week02/dom-manipulation-olympics/dom-olympics-start/index.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("header");
+header.innerHTML = "<h1>JavaScript Made This</h1><h2><span class='name'>Rob</span> wrote the JavaScript</h2>"
+
+const messages = document.getElementsByClassName("messages");
+messages.innerHTML = '<div class="message left">I have something fun and good to talk about</div><div class="message right">I don\'t want to talk</div><div class="message left">You have to, you\'re the police</div><div class="message right">I\'m just a simple farmer</div>'
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "JavaScript Made This!!";
+
+// const h2 = document.createElement("h2");
+// h2.textContent = "Rob wrote the JavaScript";
+
+// header.appendChild(h1);
+// header.appendChild(h2);
+
---
+++ exercises/week02/uptown-function-you-up/app.js
@@ -0,0 +1,65 @@
+const lyrics = ["This", "hit", "that", "ice", "cold", 
+              "Michelle", "Pfeiffer", "that", "white", 
+              "gold", "This", "one", "for", "them", 
+              "hood", "girls", "Them", "good", "girls", 
+              "straight", "masterpieces", "Stylin'", 
+              "whilen'", "livin'", "it", "up", "in", 
+              "the", "city", "Got", "Chucks", "on", 
+              "with", "Saint", "Laurent", "Gotta", "kiss", 
+              "myself", "I'm", "so", "pretty"];
+
+const sing = input => input.join(" ");
+function reverse(input) {
+    let array = input;
+    return array.reverse().join(" ");
+} 
+function everyOtherWordString(input, start) {  // i = the starting index
+    let newArray = []
+    for ( i = start; i < input.length; i += 2) {
+        newArray.push(input[i]);
+    }
+    return newArray.join(" ");
+}
+function everyOtherWord(input, start) {  // i = the starting index
+    let newArray = []
+    for ( i = start; i < input.length; i += 2) {
+        newArray.push(input[i]);
+    }
+    return newArray;
+}
+function switchEveryTwo(input) {
+    let newArray1 = everyOtherWord(input, 0);
+    let newArray2 = everyOtherWord(input, 1);
+    let finalArray = []
+    for (let i = 0; i < Math.round(input.length / 2); i++) {
+        if (newArray2[i] === undefined) {
+            finalArray.push(newArray1[i]);
+            break;
+        } else {
+            finalArray.push(newArray2[i]);
+            finalArray.push(newArray1[i]);
+        }
+    }
+    return finalArray;
+}
+function randomize(input) {
+    for (let i = input.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+    return input.join(" ");
+}
+
+console.log(`\n\nSing:\n${sing(lyrics)}`);
+console.log(`\n\nReverse:\n${reverse(lyrics)}`);
+/*
+~~~~~~~~~~~~~~~~GET HELP FOR THIS~~~~~~~~~~~~~~~~~~~~`
+Why does the reverse() function reverse the array for all following functions??????
+If I comment out the call of the reverse functions, then everything works perfectly...
+~~~~~~~~~~~~~~~~GET HELP FOR THIS~~~~~~~~~~~~~~~~~~~~`
+*/
+console.log(`\n\nEveryOtherWord:\n${everyOtherWordString(lyrics, 0)}`);
+console.log(`\n\nSwitchEveryTwo:\n${switchEveryTwo(lyrics)}`);
+console.log(`\n\nRandomize\n${randomize(lyrics)}`);
