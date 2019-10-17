c9263cef6e5a053fb7de0bc7d01bb4623dd2cb76
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-16 22:31:18 -0600

    worked on console-rpg

---
+++ exercises/week03/letter-frequency/app.js
@@ -0,0 +1,14 @@
+const phrase = 'slimy smelly solution';
+
+arrayIndex = (str) => {
+    let arr = str.split("");
+    arr.filter(char => {
+        
+    });
+
+}
+
+console.log(arrayIndex(phrase));
+
+//Output: { s: 3, l: 4, i: 2, m: 2, y: 2, ' ': 2, e: 1, o: 2, u: 1, t: 1, n: 1 }
+
--- projects/console-rpg/app.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline-sync");
-
-function wait(ms) {
-    let start = new Date().getTime();
-    let now = start;
-    while(now < start + ms) {
-        now = new Date().getTime();
-    }
-}
