a2bdacb5b5c0d21adb65e6ef06e3a3f0dbd99d44
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-23 14:53:47 -0600

    completed the-princess-is-in-another-castle

--- exercises/week04/the-princess-is-in-another-castle/app.js
+++ exercises/week04/the-princess-is-in-another-castle/app.js
@@ -1,24 +1,77 @@
 class Player{
-    constructor(name,totalCoins,status,hasStar,gameActive=true){
-        this.name = name;
-        this.totalCoins = totalCoins;
-        this.status = status;
-        this.hasStar = hasStar;
-        this.gameActive = gameActive;
-    }
-    setName(namePicked){
-        this.name = namePicked;
-    }
-    gotHit(){
-        
-    }
-    gotPowerup(){
+  constructor(name,status='Small',totalCoins=0,hasStar=false,starStartTime,gameActive=true){
+    this.name = name;
+    this.totalCoins = totalCoins;
+    this.status = status;
+    this.hasStar = hasStar;
+    this.starStartTime = starStartTime;
+    this.gameActive = gameActive;
+  };
+  setName(namePicked){
+    this.name = namePicked;
+  };
+  gotHit(){
+    //changes status -> status options: ['Powered Up' -> 'Big' -> 'Small' -> 'Dead']
+    if (this.hasStar) {
+      //hit does nothing
+    } else if (this.status === 'Powered Up'){
+      this.status = 'Big';
+    } else if (this.status === 'Big') {
+      this.status = 'Small';
+    } else if (this.status === 'Small') {
+      this.status = 'Dead';
+      this.gameActive = false;
+    };
+  };
+  gotPowerup(){
+    // 'Small' -> 'Big' -> 'Powered Up' -> change hasStar=true
+    if (this.status === 'Powered Up'){
+      this.hasStar = true;
+      //start star timer
+      this.starStartTime = new Date().getTime();
+    } else if (this.status === 'Big') {
+      this.status = 'Powered Up';
+    } else if (this.status === 'Small') {
+      this.status = 'Big';
+    };
+  };
+  addCoin(){
+    this.totalCoins++;
+  };
+  print(){
+    console.log(`\n\nName: ${this.name}\nTotal Coins: ${this.totalCoins}\nStatus: ${this.status}\nHas Star: ${this.hasStar}\n\n`);
+  };
+};
 
-    }
-    addCoin(){
+const mario = new Player('Mario');
+const luigi = new Player('Luigi');
 
-    }
-    print(){
-        console.log(`Name: ${this.name}\nTotal Coins: ${this.totalCoins}\nStatus: ${this.status}\nHas Star: ${this.hasStar}`)
-    }
+const player = mario;
+
+const myInterval = setInterval(() => {
+  play();
+  if (player.gameActive === false) {
+    clearInterval(myInterval);
+  }
+},1000);
+
+function play() {
+  checkIfStarExpired();
+  let num = Math.floor(Math.random()*3);
+  if (num === 2) {
+    player.gotHit();
+  } else if (num ===1){
+    player.gotPowerup();
+  } else {
+    player.addCoin();
+  };
+  player.print();
+}
+
+function checkIfStarExpired() {
+  let currentTime = new Date().getTime();
+  if (currentTime > player.starStartTime + 3000) {
+    player.starStartTime = 0;
+    player.hasStar = false;
+  }
 }
