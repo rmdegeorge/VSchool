0b036baa31b4dd280ec41c5cc786d622925ee2ee
Rob DeGeorge <rmdegeorge@gmail.com>
2019-10-24 11:04:57 -0600

    started business-homepage-in-react

---
+++ exercises/week04/first-react-app/src/components/myComponent/MyComponent.js
@@ -0,0 +1,17 @@
+// ./components/myComponent/MyComponent.js
+
+import React from "react";
+
+//Make a new class and extend React.Component to use reacts component code
+function MyComponent() {
+    //inside it will return some JSX
+    return (
+      <h1>
+      Hello World
+      </h1>
+    )
+}
+
+//export your component so it can be used in your app
+export default MyComponent;
+
