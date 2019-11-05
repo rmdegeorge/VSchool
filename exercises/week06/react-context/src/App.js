8367d30fd333047a265f8a21e4090238e2b4a09b
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-05 11:51:25 -0700

    started: react-context

---
+++ exercises/week06/react-context/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        I'm an App!
+      </div>
+    )
+  }
+}
