687769a23d8f8acb9e767130598ad96af9b72908
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-21 10:47:30 -0700

    broke server.js into routes

---
+++ exercises/week07/bounty-hunter/routes/bountyRoutes.js
@@ -0,0 +1,38 @@
+// bountyRoutes.js
+
+const express = require("express");
+const bountyRouter = express.Router();
+const uuid = require('uuid');
+const database = require('./bountiesData.json');
+bountyRouter.use(express.json());
+
+bountyRouter.route('/bounty')
+  .get((req,res) => {
+    res.send(database);
+  })
+  .post((req,res) => {
+    req.body._id = uuid.v4();
+    database.push(req.body);
+    res.send(req.body);
+  });
+
+bountyRouter.route('/bounty/:_id')
+  .get((req,res) => {
+    const foundBounty = database.find(bounty => bounty._id == req.params._id);
+    res.send(foundBounty);
+  })
+  .put((req,res) => {
+    let {_id} = req.params;
+    let updatedBounty = req.body;
+    database.forEach(bounty => bounty._id == _id && Object.assign(bounty, updatedBounty));
+    res.send(updatedBounty);
+  })
+  .delete((req,res) => {
+    let {_id} = req.params;
+    let index = database.findIndex(bounty => bounty._id == _id);
+    database.splice(index,1);
+    res.send('Successfully deleted Bounty');
+  });
+
+
+module.exports = bountyRouter;
--- exercises/week07/bounty-hunter/server.js
+++ exercises/week07/bounty-hunter/server.js
@@ -1,39 +1,44 @@
+// server.js
+
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const database = require('./bountiesData.json');
 app.use(express.json());
 
-app.get('/bounty', (req, res) => {
-  res.send(database);
-});
+app.use("/bounty", require("./routes/bountyRoutes"));
 
-app.get('/bounty/:_id', (req, res) => {
-  const foundBounty = database.find(bounty => bounty._id == req.params._id)
-  res.send(foundBounty);
-})
-
-app.post('/bounty', (req, res) => {
-  req.body._id = uuid.v4();
-  database.push(req.body);
-  res.send(req.body);
-});
-
-app.put('/bounty/:_id', (req, res) => {
-  let {_id} = req.params;
-  let updatedBounty = req.body;
-  database.forEach(bounty => bounty._id == _id && Object.assign(bounty, updatedBounty));
-  res.send(updatedBounty);
-});
-
-app.delete('/bounty/:_id', (req, res) => {
-  let {_id} = req.params;
-  let index = database.findIndex(bounty => bounty._id == _id);
-  database.splice(index, 1);
-  res.send(`Successfully deleted Bounty`);
-});
+// app.get('/bounty', (req, res) => {
+//   res.send(database);
+// });
+//
+// app.get('/bounty/:_id', (req, res) => {
+//   const foundBounty = database.find(bounty => bounty._id == req.params._id)
+//   res.send(foundBounty);
+// })
+//
+// app.post('/bounty', (req, res) => {
+//   req.body._id = uuid.v4();
+//   database.push(req.body);
+//   res.send(req.body);
+// });
+//
+// app.put('/bounty/:_id', (req, res) => {
+//   let {_id} = req.params;
+//   let updatedBounty = req.body;
+//   database.forEach(bounty => bounty._id == _id && Object.assign(bounty, updatedBounty));
+//   res.send(updatedBounty);
+// });
+//
+// app.delete('/bounty/:_id', (req, res) => {
+//   let {_id} = req.params;
+//   let index = database.findIndex(bounty => bounty._id == _id);
+//   database.splice(index, 1);
+//   res.send(`Successfully deleted Bounty`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
